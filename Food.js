4688e30cacf449b20dd8d1ef07164521669b1eb5
Sergio Vega <hekinky@MacBook-Pro.local>
2021-04-22 00:25:20 -0600

    [Complete]

--- Food.js
+++ Food.js
@@ -1,20 +1,28 @@
-import { onSnake, expandSnake } from './Snake.js'
+import { onSnake, expandSnake } from './snake.js'
+import { randomGridPosition } from './grid.js'
 
-let food = { x: 10, y: 1}
-const  EXPANSION_RATE = 1
-
+let food = getRandomFoodPosition()
+const EXPANSION_RATE = 4
 
 export function update() {
-    if (onSnake(food)) {
-        expandSnake(EXPANSION_RATE)
-        food = {x: 20, y: 10}
-    }
+  if (onSnake(food)) {
+    expandSnake(EXPANSION_RATE)
+    food = getRandomFoodPosition()
+  }
 }
 
 export function draw(gameBoard) {
-    const foodElement = document.createElement('div')
-    foodElement.style.gridRowStart = food.y
-    foodElement.style.gridColumnStart = food.x
-    foodElement.classList.add('food')
-    gameBoard.appendChild(foodElement)
+  const foodElement = document.createElement('div')
+  foodElement.style.gridRowStart = food.y
+  foodElement.style.gridColumnStart = food.x
+  foodElement.classList.add('food')
+  gameBoard.appendChild(foodElement)
+}
+
+function getRandomFoodPosition() {
+  let newFoodPosition
+  while (newFoodPosition == null || onSnake(newFoodPosition)) {
+    newFoodPosition = randomGridPosition()
+  }
+  return newFoodPosition
 }
